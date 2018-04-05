02c33113923972e262afd21b637fb584678fb3cb
Matt Moresco <mmoresco@appnexus.com>
2018-04-05 15:50:32 -0400

    Using client-go for linting

--- helm/lint.go
+++ helm/lint.go
@@ -2,15 +2,18 @@ package helm
 
 import (
 	"fmt"
-	"io"
+	"github.com/appnexus/ankh/context"
 	"strings"
 
-	"github.com/appnexus/ankh/context"
-	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	appsv1beta1 "k8s.io/client-go/pkg/apis/apps/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
 type Container struct {
-	Name string
+	Name  string
 	Image string
 }
 
@@ -34,122 +37,131 @@ type KubeObject struct {
 	}
 }
 
-func LintObject(ctx *ankh.ExecutionContext, ankhFile ankh.AnkhFile, obj KubeObject) []error {
+func LintObjectMeta(ctx *ankh.ExecutionContext, meta v1.ObjectMeta, kind string) []error {
+	errors := []error{}
 	release := ctx.AnkhConfig.CurrentContext.Release
 	if release == "" {
 		return []error{}
 	}
 
-	errors := []error{}
-
 	// Verify that every object has a name with `-$release` as a suffix.
 	suffix := fmt.Sprintf("-%v", release)
-	if !strings.HasSuffix(obj.Metadata.Name, suffix) {
-		e := fmt.Errorf("Object with kind '%v' and name '%v': object name is missing a dashed release suffix (in this case, '%v'). Use .Release.Name in your template to ensure that all objects are named with the release as a suffix to aovid name collisions across releases.",
-			obj.Kind, obj.Metadata.Name, suffix)
+	if !strings.HasSuffix(meta.Name, suffix) {
+		e := fmt.Errorf("Object with kind '%v' and name '%v': object name is missing a dashed release suffix "+
+			"(in this case, '%v'). Use .Release.Name in your template to ensure that all objects are named with the release "+
+			"as a suffix to aovid name collisions across releases.",
+			kind, meta.Name, suffix)
 		errors = append(errors, e)
 	}
-	ctx.Logger.Debugf("Object with kind '%v' and name '%v': object name does indeed contain the desired suffix `%v`", obj.Kind, obj.Metadata.Name, suffix)
+	ctx.Logger.Debugf("Object with kind '%v' and name '%v': object name does indeed contain the desired suffix `%v`",
+		kind, meta.Name, suffix)
 
 	// Verify that every object is labeled with a key `release` and value equal to the current context's release
-	if obj.Metadata.Labels["release"] != release {
-		e := fmt.Errorf("Object with kind '%v' and name '%v': object is missing a `release` label with the release name as a value (in this case, '%v'). Found these labels on the object: %+v", obj.Kind, obj.Metadata.Name, release, obj.Metadata.Labels)
+	if meta.Labels["release"] != release {
+		e := fmt.Errorf("Object with kind '%v' and name '%v': object is missing a `release` label with the "+
+			"release name as a value (in this case, '%v'). Found these labels on the object: %+v",
+			kind, meta.Name, release, meta.Labels)
 		errors = append(errors, e)
 	}
-	ctx.Logger.Debugf("Object with kind '%v' and name '%v': object labels exist, and the release label is '%v'", obj.Kind, obj.Metadata.Name, obj.Metadata.Labels["release"])
+	ctx.Logger.Debugf("Object with kind '%v' and name '%v': object labels exist, and the release label is '%v'",
+		kind, meta.Name, meta.Labels)
 
-	switch strings.ToLower(obj.Kind) {
-	case "deployment":
+	return errors
+}
+
+func LintObject(ctx *ankh.ExecutionContext, ankhFile ankh.AnkhFile, obj runtime.Object) []error {
+	errors := []error{}
+	release := ctx.AnkhConfig.CurrentContext.Release
+
+	switch obj.(type) {
+
+	case *appsv1beta1.Deployment:
+		deployment := obj.(*appsv1beta1.Deployment)
+
+		metaErrors := LintObjectMeta(ctx, deployment.ObjectMeta, deployment.Kind)
+		if len(metaErrors) > 0 {
+			errors = append(errors, metaErrors...)
+		}
+
 		// The Deployment should create pods with the `release` label
-		if obj.Spec.Template.Metadata.Labels["release"] != release {
-			e := fmt.Errorf("Deployment with name '%v': object's spec.template.metadata.labels is missing a `release` label with the release name as a value (in this case, '%v'). Found these labels on spec.template.metadata: %+v", obj.Kind, obj.Metadata.Name, release, obj.Spec.Template.Metadata.Labels)
+		if deployment.Spec.Template.ObjectMeta.Labels["release"] != release {
+			e := fmt.Errorf("Deployment with name '%v': object's spec.template.metadata.labels is missing a `release` " +
+				"label with the release name as a value (in this case, '%v'). Found these labels on spec.template.metadata: %+v",
+					deployment.Kind, deployment.ObjectMeta.Name, release, deployment.Spec.Template.ObjectMeta.Labels)
 			errors = append(errors, e)
 		}
-		ctx.Logger.Debugf("Deployment with name '%v': object spec.template.metadata.labels exists, and the release label is %v", obj.Kind, obj.Metadata.Name, obj.Spec.Template.Metadata.Labels["release"])
-	case "service":
-		// If the Service is not targeting an ExternalName, it should target pods with a `release` label
-		if obj.Spec.Type != "ExternalName" {
-			if obj.Spec.Selector["release"] != release {
-				e := fmt.Errorf("Service with type '%v' and name '%v': object's spec.selector is missing the `release` key with the release name as a value (in this case, '%v'). Found these keys on spec.selector: %+v", obj.Spec.Type, obj.Metadata.Name, release, obj.Spec.Selector)
-				errors = append(errors, e)
+		ctx.Logger.Debugf("Deployment with name '%v': object spec.template.metadata.labels exists, and the release " +
+			"label is %v", deployment.Kind, deployment.ObjectMeta.Name, deployment.Spec.Template.ObjectMeta.Labels["release"])
+
+	case *apiv1.Service:
+		service := obj.(*apiv1.Service)
+
+		metaErrors := LintObjectMeta(ctx, service.ObjectMeta, service.Kind)
+		if len(metaErrors) > 0 {
+			errors = append(errors, metaErrors...)
+		}
+
+			// If the Service is not targeting an ExternalName, it should target pods with a `release` label
+			if service.Spec.Type != "ExternalName" {
+				if service.Spec.Selector["release"] != release {
+					e := fmt.Errorf("Service with type '%v' and name '%v': object's spec.selector is missing the `release` " +
+						"key with the release name as a value (in this case, '%v'). Found these keys on spec.selector: %+v",
+							service.Spec.Type, service.ObjectMeta.Name, release, service.Spec.Selector)
+					errors = append(errors, e)
+				}
+				ctx.Logger.Debugf("Service with type '%v' and name '%v': object spec.selector exists, and the release " +
+					"key is %v", service.Spec.Type, service.ObjectMeta.Name, service.Spec.Selector["release"])
 			}
-			ctx.Logger.Debugf("Service with type '%v' and name '%v': object spec.selector exists, and the release key is %v", obj.Spec.Type, obj.Metadata.Name, obj.Spec.Selector["release"])
+	}
+
+	if ctx.Pedantic {
+		pedanticErrors := PedanticLintObject(ctx, ankhFile, obj)
+		if len(pedanticErrors) > 0 {
+			errors = append(errors, pedanticErrors...)
 		}
 	}
 
 	return errors
 }
 
-func PedanticLintObject(ctx *ankh.ExecutionContext, ankhFile ankh.AnkhFile, obj KubeObject) []error {
+func PedanticLintObject(ctx *ankh.ExecutionContext, ankhFile ankh.AnkhFile, obj runtime.Object) []error {
 	errors := []error{}
-	switch strings.ToLower(obj.Kind) {
-	case "deployment":
-		for _, c := range obj.Spec.Template.Spec.Containers {
-			if !strings.HasPrefix(c.Image, "TODO REPO") {
-				// TODO: stick this in a conf file
-				e := fmt.Errorf("[Pedantic] Deployment with name '%v': has container '%v' with image '%v'. Deployment " +
-					"image must point to 'TODO REPO'", obj.Metadata.Name, c.Name, c.Image)
-				errors = append(errors, e)
+	switch obj.(type) {
+	case *appsv1beta1.Deployment:
+		deployment := obj.(*appsv1beta1.Deployment)
+			for _, c := range deployment.Spec.Template.Spec.Containers {
+				if !strings.HasPrefix(c.Image, "TODO REPO") {
+					// TODO: stick this in a conf file
+					e := fmt.Errorf("[Pedantic] Deployment with name '%v' has container '%v' with image '%v'. Deployment "+
+						"image must point to 'TODO REPO'", deployment.ObjectMeta.Name, c.Name, c.Image)
+					errors = append(errors, e)
+					ctx.Logger.Debugf("Deployment with name '%v' has container '%v' with image '%v' correctly pointing to " +
+						"'%v", deployment.ObjectMeta.Name, c.Name, c.Image, "TODO REPO")
+				}
 			}
 		}
-	}
+
 	return errors
-}
-
-func PedanticLint(ctx *ankh.ExecutionContext, ankhFile ankh.AnkhFile, objects []KubeObject) [] error {
-	allErrors := []error{}
-	ctx.Logger.Debugf("[Pedantic] Linting %v", ankhFile.Path)
-	hasService := false
-
-	for _, obj := range objects {
-		if obj.Kind == "Service" {
-			hasService = true
-		}
-		errors := PedanticLintObject(ctx, ankhFile, obj)
-		if len(errors) > 0 {
-			allErrors = append(allErrors, errors...)
-		}
-	}
-
-	// Verify chart has a service object
-	if !hasService {
-		e := fmt.Errorf("Chart is missing a service object")
-		allErrors = append(allErrors, e)
-	}
-	return allErrors
 }
 
 func Lint(ctx *ankh.ExecutionContext, helmOutput string, ankhFile ankh.AnkhFile) []error {
-	decoder := yaml.NewDecoder(strings.NewReader(helmOutput))
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	parts := strings.Split(helmOutput, "---")
 
 	allErrors := []error{}
-	objects := []KubeObject{}
 
 	ctx.Logger.Debugf("Linting %v", ankhFile.Path)
-	for {
-		obj := KubeObject{}
-		err := decoder.Decode(&obj)
-		if err == io.EOF {
-			break
-		}
+	for _, part := range parts[1:] {
+		obj, _, err := decode([]byte(part), nil, nil)
 
-		ctx.Logger.Debugf("Decoded a kube object with kind '%v'", obj.Kind)
-		if obj.Kind == "" {
-			// Ignore empty documents
+		if runtime.IsMissingKind(err) {
 			ctx.Logger.Debugf("Skipping empty document")
 			continue
 		}
 
-		objects = append(objects, obj)
+		ctx.Logger.Debugf("Decoded a kube object with kind '%v'", obj.GetObjectKind().GroupVersionKind().Kind)
 
 		errors := LintObject(ctx, ankhFile, obj)
-		if len(errors) > 0 {
-			allErrors = append(allErrors, errors...)
-		}
-	}
-
-	if ctx.Pedantic {
-		errors := PedanticLint(ctx, ankhFile, objects)
 		if len(errors) > 0 {
 			allErrors = append(allErrors, errors...)
 		}
