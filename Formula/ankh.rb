620c776706f494edb65fcddf80bfe5418835736d
Steven E. Newton <snewton@appnexus.com>
2019-01-28 12:36:00 -0800

    Support for macOS Homebrew https://brew.sh/
    Binary-only for now
    brew tap appnexus/ankh git@github.com:appnexus/ankh.git

--- Formula/ankh.rb
+++ Formula/ankh.rb
@@ -1,8 +1,8 @@
 class Ankh < Formula
   desc "Another Kubernetes Helper"
   homepage "https://github.com/appnexus/ankh"
-  url "https://github.com/appnexus/ankh/releases/download/${version}/ankh-darwin-amd64.tar.gz"
   version "2.0.0-beta.4"
+  url "https://github.com/appnexus/ankh/releases/download/${version}/ankh-darwin-amd64.tar.gz"
   sha256 "7e9879a0c69ad9ae4135e9f3ab22221dd70bcec0f5e6feafaab2d4dd4a227701"
 
   def install
